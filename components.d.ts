91bc303e4db70bee8eacb5195b7ab0cdcd47edb1
XTsuola <86823660+XTsuola@users.noreply.github.com>
2025-05-15 18:02:05 +0800

    update

--- components.d.ts
+++ components.d.ts
@@ -23,6 +23,8 @@ declare module 'vue' {
     AModal: typeof import('ant-design-vue/es')['Modal']
     APagination: typeof import('ant-design-vue/es')['Pagination']
     APopconfirm: typeof import('ant-design-vue/es')['Popconfirm']
+    ARadioButton: typeof import('ant-design-vue/es')['RadioButton']
+    ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
     ASelect: typeof import('ant-design-vue/es')['Select']
     ASelectOption: typeof import('ant-design-vue/es')['SelectOption']
     ASubMenu: typeof import('ant-design-vue/es')['SubMenu']
