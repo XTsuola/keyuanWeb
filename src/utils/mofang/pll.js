dc465ac5de0d77436a05415f8770ff21234291aa
XTsuola <86823660+XTsuola@users.noreply.github.com>
2026-03-11 13:49:45 +0800

    222

---
+++ src/utils/mofang/pll.js
@@ -0,0 +1,48 @@
+/*
+第四步：顶层复原（pll）
+
+1.Aa：
+
+2.Ab：
+
+3.E：x'RU'R'DRUR'D'RUR'DRU'R'D'x 向下翻转（上右下右 上左下左 上左下右 上右下左）向上翻转
+ 
+4.F：R'U'F'RUR'U'R'FR2U'R'U'RUR'UR（下回 提上 勾下回下 压上上 回下回上 勾下勾上）
+
+5.Ga：
+
+6.Gb：R'U'RUD'R2UR'URU'RU'R'2D（下右上左 左上上 左下左上 右上右下下 右）
+
+7.Gc：R2U'RU'RUR'URRD'URU'R'D（上上 右上右上 左下左上 上左左上右下右）
+
+8.Gd：RU'R'U'RURDR'U'RD'R'U2R'（上右下右 上左下右 下右上左 下左左下）
+
+9.H：M2U'M2U'2M2U'M2（上上右 上上右右 上上右 上上）
+
+10.Ja：L'U'LFL'U'LULF'L'2UL
+
+11.Jb：RUR'F'RUR'U'R'FR2U'R'（上勾下 提上 勾下回下 压上上回下）
+
+12.Na：LU'RU'2L'UR'LU'R'U'2L'UL 左手（下勾上勾勾上回下 下勾上勾勾上回下）
+
+13.Nb：R'UL'U2RU'LR'UL'U2RU'L（下勾上勾勾上回下 下勾上勾勾上回下）
+
+14.Ra：
+
+15.Rb：
+
+16.T：RUR'U'R'FR2U'R'U'RUR'F'（上左下右 下压上上 右下右上左下 提）
+
+17.Ua：RU'RURURU'R'U'R2（上右上左 上左上右 下右上上）
+
+18.Ub：R2URUR'U'R'U'R'UR'（上上左 上左下右 下右下左下）
+
+19.V：R'UR'U'RD'R'DR'UD'R2U'R'2DR2（下左下右 上左下右 下左上上右下下右上上）
+
+20.Y：R2U'R2U'R2UFUF'R2FU'F'（上上右 上上右 上上左 压左提 上上 压右提）
+
+21.Z：M'U'M'2U'M'2U'M'U'2M'2U（上右 上上右 上上右 上右右 上上左）
+
+
+
+*/
