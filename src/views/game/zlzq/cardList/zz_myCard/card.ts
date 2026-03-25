7671266456b109a9bf84fad22f4ab144404e45cd
XTsuola <86823660+XTsuola@users.noreply.github.com>
2026-03-25 11:32:59 +0800

    12

--- src/views/game/zlzq/cardList/zz_myCard/card.ts
+++ src/views/game/zlzq/cardList/zz_myCard/card.ts
@@ -1,4 +1,4 @@
-// 帝国：（15张）、（19.60级）
+// 帝国：（15张）、（19.27级）
 export const simangdiguo = [
     [ // 蓝卡：（6张）、（22.00级）
         { cost: 1, name: "圣殿斥候", level: 22 },
@@ -8,23 +8,23 @@ export const simangdiguo = [
         { cost: 3, name: "增援战线", level: 22 },
         { cost: 5, name: "光明惩戒", level: 22 },
     ],
-    [ // 紫卡：（5张）、（17.00级）
+    [ // 紫卡：（4张）、（16.75级）
         { cost: 1, name: "四芒军旗", level: 18 },
         { cost: 2, name: "圣殿御卫", level: 22 },
-        { cost: 4, name: "召集护卫", level: 18 },
         { cost: 4, name: "夺取阵地", level: 12 },
         { cost: 6, name: "惩戒天使", level: 15 },
     ],
-    [ // 橙卡：（4张）、（19.25级）
-        { cost: 3, name: "曙光·安娜贝尔", level: 19 },
+    [ // 橙卡：（5张）、（18.00级）
+        { cost: 3, name: "曙光·安娜贝尔", level: 20 },
         { cost: 4, name: "圣枪·卡洛琳", level: 20 },
         { cost: 4, name: "明日之音·露娜", level: 18 },
-        { cost: 6, name: "正阳大主教·伊恩", level: 20 }
+        { cost: 6, name: "正阳大主教·伊恩", level: 20 },
+        { cost: 8, name: "钢铁统帅·雷蒙德", level: 12 },
     ]
 ];
 
 
-// 隐秘：（16张）、（18.94级）
+// 隐秘：（16张）、（19.19级）
 export const yinmizhe = [
     [ // 蓝卡：（5张）、（19.20级）
         { cost: 0, name: "洞悉之眼", level: 12 },
@@ -33,9 +33,9 @@ export const yinmizhe = [
         { cost: 2, name: "沉重否定", level: 18 },
         { cost: 3, name: "全数否定", level: 22 },
     ],
-    [ // 紫卡：（9张）、（18.78级）
-        { cost: 1, name: "隐形术", level: 15 },
-        { cost: 2, name: "破魔系教授", level: 21 },
+    [ // 紫卡：（9张）、（19.22级）
+        { cost: 1, name: "隐形术", level: 18 },
+        { cost: 2, name: "破魔系教授", level: 22 },
         { cost: 2, name: "克隆术1", level: 16 },
         { cost: 2, name: "克隆术2", level: 14 },
         { cost: 3, name: "学仆-脉冲型1", level: 22 },
@@ -51,16 +51,16 @@ export const yinmizhe = [
 ];
 
 
-// 港口：（15张）、（15.00级）
+// 港口：（15张）、（15.13级）
 export const tiantanggang = [
-    [ // 蓝卡：（8张）、（16.25级）
-        { cost: 1, name: "心灵黑洞1", level: 15 },
+    [ // 蓝卡：（8张）、（16.50级）
+        { cost: 1, name: "心灵黑洞1", level: 16 },
         { cost: 1, name: "心灵黑洞2", level: 14 },
         { cost: 1, name: "心灵黑洞3", level: 13 },
         { cost: 2, name: "狡诈学徒", level: 20 },
         { cost: 2, name: "黑夜突袭", level: 20 },
         { cost: 2, name: "黑金勒索者", level: 12 },
-        { cost: 3, name: "热血矿工", level: 20 },
+        { cost: 3, name: "热血矿工", level: 21 },
         { cost: 3, name: "不法交易", level: 16 },
     ],
     [ // 紫卡：（4张）、（14.75级）
